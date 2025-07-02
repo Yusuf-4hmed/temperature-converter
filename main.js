c86ba4fdbfecb9a107130b381d572a29a03b7ac2
Yusuf Ahmed <yusufahmed0625@gmail.com>
2025-07-02 21:39:37 +0100

    implimented regex validation to input fields

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const tempInput = document.getElementById('temp-input')
 const tempOutupt = document.getElementById('temp-output')
 
-const tempInputRegex = /^\d*\.?\d*$/
+const tempInputRegex = /^\-?\d*\.?\d*$/
 
 const celciusToFarenheit = () => {
     tempOutupt.value = Math.round((tempInput.value * 9/5) + 32)
@@ -10,9 +10,18 @@ const farenheitToCelcius = () => {
     tempInput.value = Math.round((tempOutupt.value - 32) * 5/9)
 }
 
-tempInput.addEventListener('input', () => {
-    celciusToFarenheit()
+tempInput.addEventListener('input', (e) => {
+    if (!tempInputRegex.test(tempInput.value)) {
+        e.target.value = e.target.value.slice(0, -1)
+    } else {
+     celciusToFarenheit()   
+    }
+    
 })
-tempOutupt.addEventListener('input', () => {
-    farenheitToCelcius()
+tempOutupt.addEventListener('input', (e) => {
+    if (!tempInputRegex.test(tempOutupt.value)) {
+        e.target.value = e.target.value.slice(0, -1)
+    } else {
+     farenheitToCelcius()   
+    }
 })
