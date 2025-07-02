e5eb9174b2124cb5f94086cc9374a66a3079bc9f
Yusuf Ahmed <yusufahmed0625@gmail.com>
2025-07-02 21:30:49 +0100

    added logic for farenheit to celcius

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 const tempInput = document.getElementById('temp-input')
 const tempOutupt = document.getElementById('temp-output')
 
-
+const tempInputRegex = /^\d*\.?\d*$/
 
 const celciusToFarenheit = () => {
-    tempOutupt.innerText = ((tempInput.value * 9/5) + 32)
+    tempOutupt.value = Math.round((tempInput.value * 9/5) + 32)
+}
+const farenheitToCelcius = () => {
+    tempInput.value = Math.round((tempOutupt.value - 32) * 5/9)
 }
 
 tempInput.addEventListener('input', () => {
     celciusToFarenheit()
+})
+tempOutupt.addEventListener('input', () => {
+    farenheitToCelcius()
 })
